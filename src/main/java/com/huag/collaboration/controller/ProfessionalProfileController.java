dbcab14bbfd219eddee1183d66e6e4ab1d82cb29
huag <18600132770@163.com>
2019-10-28 17:38:06 +0800

    修改了上下序dialog的bug

--- src/main/java/com/huag/collaboration/controller/ProfessionalProfileController.java
+++ src/main/java/com/huag/collaboration/controller/ProfessionalProfileController.java
@@ -1,10 +1,8 @@
 package com.huag.collaboration.controller;
 
 import com.huag.collaboration.entities.ProfessionalProfile;
-import com.huag.collaboration.entities.UpperProfessionData;
 import com.huag.collaboration.entities.query.BaseResponse;
 import com.huag.collaboration.mapper.ProfessionalProfileMapper;
-import com.huag.collaboration.mapper.UpperProfessionDataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
