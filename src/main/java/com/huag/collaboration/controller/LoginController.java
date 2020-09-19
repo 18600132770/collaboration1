8d728d6b5809983f7c5cd05ff79d243ea0285ccb
zhangwenhua <zhangwenhua03@meituan.com>
2020-09-19 16:00:08 +0800

    修改登录bug

--- src/main/java/com/huag/collaboration/controller/LoginController.java
+++ src/main/java/com/huag/collaboration/controller/LoginController.java
@@ -1,14 +1,23 @@
 package com.huag.collaboration.controller;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.github.pagehelper.util.StringUtil;
+import com.huag.collaboration.entities.TaskAssignment;
 import com.huag.collaboration.entities.User;
+import com.huag.collaboration.entities.base.PageBaseResponse;
+import com.huag.collaboration.entities.query.BaseResponse;
 import com.huag.collaboration.mapper.UserMapper;
+import com.huag.collaboration.utils.RequestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,20 +46,23 @@ public class LoginController {
         }
         if("admin".endsWith(username) && "123456".equals(password)){
             session.setAttribute("loginUser", username);
-            return "redirect:/main.html";
+            //return "redirect:/main.html";
         }else if(flag){
             session.setAttribute("loginUser", user.getCardId());
-            return "redirect:/main.html";
+            //return "redirect:/main.html";
         }else{
             map.put("msg", "用户名或密码错误");
             return "login";
         }
+        return null;
     }
 
     @GetMapping("/redirectToMain")
     public String redirectToMain(){
         return "redirect:/main.html";
     }
+
+
 
 }
 
